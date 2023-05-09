f03a098bcaa2a77a36c147b9f19f83cedf59a0f0
Tiziano <t.talini@libero.it>
2023-05-09 18:45:08 +0200

    layout + pagina corsi + stili

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,29 @@
 import { onMount } from "svelte";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
-onMount(async () => {
-    const bootstrap = await import('bootstrap')
-})
+import '$css/global.scss';
 
 </script>
 
-<div class="p-5">
-    <slot></slot>
+
+<div class="row">
+    <div class="sidebar position-relative">
+    </div>
+    <div class="col pe-4">
+        <slot></slot>
+    </div>
+
 </div>
+
+<style lang="scss">
+    @import '$css/variables.scss';
+    .sidebar{
+        width: 270px;
+        height: 100vh;
+        background-color: $light;
+        border-right: 1px solid $dark;
+        resize: horizontal;
+        min-width: 200px;
+        max-width: 350px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 import Icon from '$lib/components/Icon.svelte';
 import Course from '$lib/components/Course.svelte';
+import { view } from '$lib/stores';
+
 
 let courses = [
     {
@@ -28,9 +30,35 @@ let courses = [
         CFU: '9'
     }
 ]
-
 </script>
 
-{#each courses as course}
-    <Course {course} class="mb-3"/>
-{/each}
+
+<div class="d-flex flex-column content">
+    <div class="d-flex align-items-center my-5">
+        <h2 class="ms-auto me-auto display-2">I Miei Corsi</h2>
+        <i class="bi bi-gear display-6"></i>
+    </div>
+        <div class="d-flex mb-5">
+            <!--searchbar-->
+            <div class="ms-auto btn-group" role="group">
+                <input type="radio" class="btn-check" name="view" id="btnradio1" autocomplete="off" value="list" class:active={$view == 'list'} bind:group={$view}>
+                <label class="btn btn-outline-primary" for="btnradio1"><i class="bi bi-list"></i></label>
+        
+                <input type="radio" class="btn-check" name="view" id="btnradio2" autocomplete="off" value="grid" class:active={$view == 'grid'} bind:group={$view}>
+                <label class="btn btn-outline-primary" for="btnradio2"><i class="bi bi-border-all"></i></label>
+
+                <input type="radio" class="btn-check" name="view" id="btnradio3" autocomplete="off" value="graph" class:active={$view == 'graph'} bind:group={$view}>
+                <label class="btn btn-outline-primary" for="btnradio3"><i class="bi bi-diagram-3-fill"></i></label>
+            </div>
+        </div>
+        {#each courses as course}
+            <Course {course} class="mb-3"/>
+        {/each}
+</div>
+
+<style>
+    .content{
+        padding-left: 5rem;
+        padding-right: 5rem;
+    }
+</style>
