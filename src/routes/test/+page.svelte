7c4e7d85fb4f6eef1e9b7cea671caf62653ca0f1
Tiziano <t.talini@libero.it>
2023-05-17 16:45:41 +0200

    markdown + fix mongo

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,20 +1,24 @@
 <script>
     import { onMount } from "svelte";
+import Markdown from 'markdown-it';
 
-
-let file, text;
+let file;
 
 onMount(async () => {
-    reload()
+    console.log(Markdown)
+    const md = new Markdown()
+    await reload()
+    console.log(file)
+    file = md.render(file.file)
+    console.log(file)
 })
     async function reload(){
-        const resp = await fetch('/admin/api/file');
-        console.log(resp)
+        const resp = await fetch('/test2?s=true');
         file = (resp.ok && await resp.json())
     }
 
 </script>
 
 <div>
-    {file}
+    {@html file}
 </div>
