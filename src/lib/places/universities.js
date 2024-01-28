d447726f2ddddb5eb063450a7ec8f306aefc6921
Falesteo <longoa02@gmail.com>
2024-01-28 22:26:27 +0100

    - aggiunto il database per le università

---
+++ src/lib/places/universities.js
@@ -0,0 +1,658 @@
+export const universities = [
+    {
+        "name": "Università degli studi di Torino",
+        "type": "Università",
+        "province": "Torino",
+        "region": "Piemonte"
+    },
+    {
+        "name": "Politecnico di Torino",
+        "type": "Politecnico",
+        "province": "Torino",
+        "region": "Piemonte"
+    },
+    {
+        "name": "Istituto Superiore di Educazione Fisica di Torino",
+        "type": "ISEF",
+        "province": "Torino",
+        "region": "Piemonte"
+    },
+    {
+        "name": "Università degli studi del Piemonte orientale Amedeo Avogadro",
+        "type": "Università",
+        "province": "Vercelli",
+        "region": "Piemonte"
+    },
+    {
+        "name": "Università di Scienze Gastronomiche",
+        "type": "Università",
+        "province": "Cuneo",
+        "region": "Piemonte"
+    },
+    {
+        "name": "Università della Valle d'Aosta",
+        "type": "Università",
+        "province": "Aosta",
+        "region": "Valle D'aosta"
+    },
+    {
+        "name": "Università degli studi di Genova",
+        "type": "Università",
+        "province": "Genova",
+        "region": "Liguria"
+    },
+    {
+        "name": "Università Carlo Cattaneo (LIUC)",
+        "type": "Università",
+        "province": "Varese",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Università degli studi dell' Insubria",
+        "type": "Università",
+        "province": "Varese",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Università telematica e-Campus di Novedrate (CO)",
+        "type": "Università telematica",
+        "province": "Como",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Università degli studi di Milano",
+        "type": "Università",
+        "province": "Milano",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Politecnico di Milano",
+        "type": "Politecnico",
+        "province": "Milano",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Università commerciale Luigi Bocconi di Milano",
+        "type": "Università",
+        "province": "Milano",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Università Cattolica del Sacro Cuore",
+        "type": "Università",
+        "province": "Milano",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Libera Università di Lingue e Comunicazione (IULM)",
+        "type": "Università",
+        "province": "Milano",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Istituto Superiore di Educazione Fisica di Milano",
+        "type": "ISEF",
+        "province": "Milano",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Istituto Superiore di Educazione Fisica della Lombardia",
+        "type": "ISEF",
+        "province": "Milano",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Libera Università, Vita-Salute San Raffaele di Milano",
+        "type": "Università",
+        "province": "Milano",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Università degli studi di Milano-Bicocca",
+        "type": "Università",
+        "province": "Milano",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Humanitas University",
+        "type": "Università",
+        "province": "Milano",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Università degli studi di Bergamo",
+        "type": "Università",
+        "province": "Bergamo",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Università degli studi di Brescia",
+        "type": "Università",
+        "province": "Brescia",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Università degli studi di Pavia",
+        "type": "Università",
+        "province": "Pavia",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Istituto universitario di studi superiori di Pavia",
+        "type": "Scuola Superiore ad Ordinamento Speciale",
+        "province": "Pavia",
+        "region": "Lombardia"
+    },
+    {
+        "name": "Libera Università di Bolzano",
+        "type": "Università",
+        "province": "Bolzano",
+        "region": "Trentino Alto Adige"
+    },
+    {
+        "name": "Università degli studi di Trento",
+        "type": "Università",
+        "province": "Trento",
+        "region": "Trentino Alto Adige"
+    },
+    {
+        "name": "Università degli studi di Verona",
+        "type": "Università",
+        "province": "Verona",
+        "region": "Veneto"
+    },
+    {
+        "name": "Università Ca' Foscari Venezia",
+        "type": "Università",
+        "province": "Venezia",
+        "region": "Veneto"
+    },
+    {
+        "name": "Università Iuav di Venezia",
+        "type": "Università",
+        "province": "Venezia",
+        "region": "Veneto"
+    },
+    {
+        "name": "Università degli studi di Padova",
+        "type": "Università",
+        "province": "Padova",
+        "region": "Veneto"
+    },
+    {
+        "name": "Università degli studi di Udine",
+        "type": "Università",
+        "province": "Udine",
+        "region": "Friuli Venezia Giulia"
+    },
+    {
+        "name": "Università degli studi di Trieste",
+        "type": "Università",
+        "province": "Trieste",
+        "region": "Friuli Venezia Giulia"
+    },
+    {
+        "name": "Scuola internazionale superiore di studi avanzati di Trieste",
+        "type": "Scuola Superiore ad Ordinamento Speciale",
+        "province": "Trieste",
+        "region": "Friuli Venezia Giulia"
+    },
+    {
+        "name": "Università degli studi di Parma",
+        "type": "Università",
+        "province": "Parma",
+        "region": "Emilia Romagna"
+    },
+    {
+        "name": "Università degli studi di Modena e Reggio Emilia",
+        "type": "Università",
+        "province": "Modena",
+        "region": "Emilia Romagna"
+    },
+    {
+        "name": "Università degli studi di Bologna",
+        "type": "Università",
+        "province": "Bologna",
+        "region": "Emilia Romagna"
+    },
+    {
+        "name": "Istituto Superiore di Educazione Fisica di Bologna",
+        "type": "ISEF",
+        "province": "Bologna",
+        "region": "Emilia Romagna"
+    },
+    {
+        "name": "Università degli studi di Ferrara",
+        "type": "Università",
+        "province": "Ferrara",
+        "region": "Emilia Romagna"
+    },
+    {
+        "name": "Università degli studi Carlo Bo di Urbino",
+        "type": "Università",
+        "province": "Pesaro E Urbino",
+        "region": "Marche"
+    },
+    {
+        "name": "Istituto Superiore di Educazione Fisica di Urbino",
+        "type": "ISEF",
+        "province": "Pesaro E Urbino",
+        "region": "Marche"
+    },
+    {
+        "name": "Università Politecnica delle Marche - Ancona",
+        "type": "Università",
+        "province": "Ancona",
+        "region": "Marche"
+    },
+    {
+        "name": "Università degli studi di Macerata",
+        "type": "Università",
+        "province": "Macerata",
+        "region": "Marche"
+    },
+    {
+        "name": "Università degli studi di Camerino",
+        "type": "Università",
+        "province": "Macerata",
+        "region": "Marche"
+    },
+    {
+        "name": "Scuola IMT Alti Studi di Lucca",
+        "type": "Scuola Superiore ad Ordinamento Speciale",
+        "province": "Lucca",
+        "region": "Toscana"
+    },
+    {
+        "name": "Università degli studi di Firenze",
+        "type": "Università",
+        "province": "Firenze",
+        "region": "Toscana"
+    },
+    {
+        "name": "Istituto Superiore di Educazione Fisica di Firenze",
+        "type": "ISEF",
+        "province": "Firenze",
+        "region": "Toscana"
+    },
+    {
+        "name": "Istituto Italiano di Scienze Umane di Firenze",
+        "type": "Scuola Superiore ad Ordinamento Speciale",
+        "province": "Firenze",
+        "region": "Toscana"
+    },
+    {
+        "name": "Università telematica Italian University line di Firenze",
+        "type": "Università telematica",
+        "province": "Firenze",
+        "region": "Toscana"
+    },
+    {
+        "name": "Università degli studi di Pisa",
+        "type": "Università",
+        "province": "Pisa",
+        "region": "Toscana"
+    },
+    {
+        "name": "Scuola normale superiore di Pisa",
+        "type": "Scuola Superiore ad Ordinamento Speciale",
+        "province": "Pisa",
+        "region": "Toscana"
+    },
+    {
+        "name": "Scuola superiore di studi universitari e perfezionamento S. Anna di Pisa",
+        "type": "Scuola Superiore ad Ordinamento Speciale",
+        "province": "Pisa",
+        "region": "Toscana"
+    },
+    {
+        "name": "Università degli studi di Siena",
+        "type": "Università",
+        "province": "Siena",
+        "region": "Toscana"
+    },
+    {
+        "name": "Università per stranieri di Siena",
+        "type": "Università",
+        "province": "Siena",
+        "region": "Toscana"
+    },
+    {
+        "name": "Università degli studi di Perugia",
+        "type": "Università",
+        "province": "Perugia",
+        "region": "Umbria"
+    },
+    {
+        "name": "Istituto Superiore di Educazione Fisica di Perugia",
+        "type": "ISEF",
+        "province": "Perugia",
+        "region": "Umbria"
+    },
+    {
+        "name": "Università per stranieri di Perugia",
+        "type": "Università",
+        "province": "Perugia",
+        "region": "Umbria"
+    },
+    {
+        "name": "Università degli Studi della Tuscia",
+        "type": "Università",
+        "province": "Viterbo",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università degli studi di Roma La Sapienza",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università degli studi di Roma Tor Vergata",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Libera Università Maria SS.Assunta - (LUMSA) di Roma",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "LUISS - Libera Università internazionale degli studi sociali Guido Carli di Roma",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università degli studi di Roma Foro Italico",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università degli studi Roma Tre",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università Campus Bio-Medico di Roma",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università degli Studi Internazionali di Roma à UNINT",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università telematica Guglielmo Marconi di Roma",
+        "type": "Università telematica",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università telematica Unitelma Sapienza di Roma",
+        "type": "Università telematica",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università Europea di Roma",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università telematica internazionale UNINETTUNO di Roma",
+        "type": "Università telematica",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Universitas telematica Mercatorum di Roma",
+        "type": "Università telematica",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università telematica Niccolà Cusano di Roma",
+        "type": "Università telematica",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università telematica San Raffaele di Roma - già UNITEL",
+        "type": "Università telematica",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Link Campus University di Roma",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Saint Camillus International University of Health",
+        "type": "Università",
+        "province": "Roma",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università degli Studi di Cassino e del Lazio Meridionale",
+        "type": "Università",
+        "province": "Frosinone",
+        "region": "Lazio"
+    },
+    {
+        "name": "Università degli Studi del Sannio",
+        "type": "Università",
+        "province": "Benevento",
+        "region": "Campania"
+    },
+    {
+        "name": "Università telematica Giustino Fortunato di Benevento",
+        "type": "Università telematica",
+        "province": "Benevento",
+        "region": "Campania"
+    },
+    {
+        "name": "Università degli studi di Napoli Federico II",
+        "type": "Università",
+        "province": "Napoli",
+        "region": "Campania"
+    },
+    {
+        "name": "Università degli studi di Napoli - Parthenope",
+        "type": "Università",
+        "province": "Napoli",
+        "region": "Campania"
+    },
+    {
+        "name": "Università degli studi L'Orientale di Napoli",
+        "type": "Università",
+        "province": "Napoli",
+        "region": "Campania"
+    },
+    {
+        "name": "Università degli studi Suor Orsola Benincasa di Napoli",
+        "type": "Università",
+        "province": "Napoli",
+        "region": "Campania"
+    },
+    {
+        "name": "Istituto Superiore di Educazione Fisica di Napoli",
+        "type": "ISEF",
+        "province": "Napoli",
+        "region": "Campania"
+    },
+    {
+        "name": "Università degli studi della Campania Luigi Vanvitelli",
+        "type": "Università",
+        "province": "Caserta",
+        "region": "Campania"
+    },
+    {
+        "name": "Università telematica Pegaso di Napoli",
+        "type": "Università telematica",
+        "province": "Napoli",
+        "region": "Campania"
+    },
+    {
+        "name": "Università degli studi di Salerno",
+        "type": "Università",
+        "province": "Salerno",
+        "region": "Campania"
+    },
+    {
+        "name": "Università degli studi di L'Aquila",
+        "type": "Università",
+        "province": "L'aquila",
+        "region": "Abruzzo"
+    },
+    {
+        "name": "Istituto Superiore di Educazione Fisica di L'Aquila",
+        "type": "ISEF",
+        "province": "L'aquila",
+        "region": "Abruzzo"
+    },
+    {
+        "name": "Gran Sasso Science Institute - Scuola di dottorato internazionale dell'Aquila",
+        "type": "Scuola Superiore ad Ordinamento Speciale",
+        "province": "L'aquila",
+        "region": "Abruzzo"
+    },
+    {
+        "name": "Università degli studi di Teramo",
+        "type": "Università",
+        "province": "Teramo",
+        "region": "Abruzzo"
+    },
+    {
+        "name": "Università degli studi Gabriele D'Annunzio di Chieti e Pescara",
+        "type": "Università",
+        "province": "Chieti",
+        "region": "Abruzzo"
+    },
+    {
+        "name": "Università telematica non statale Leonardo da Vinci di Torrevecchia Teatina (CH)",
+        "type": "Università telematica",
+        "province": "Chieti",
+        "region": "Abruzzo"
+    },
+    {
+        "name": "Università degli studi del Molise",
+        "type": "Università",
+        "province": "Campobasso",
+        "region": "Molise"
+    },
+    {
+        "name": "Università degli studi di Foggia",
+        "type": "Università",
+        "province": "Foggia",
+        "region": "Puglia"
+    },
+    {
+        "name": "Università degli studi di Bari",
+        "type": "Università",
+        "province": "Bari",
+        "region": "Puglia"
+    },
+    {
+        "name": "Politecnico di Bari",
+        "type": "Politecnico",
+        "province": "Bari",
+        "region": "Puglia"
+    },
+    {
+        "name": "Libera Università Mediterranea LUM Giuseppe Degennaro",
+        "type": "Università",
+        "province": "Bari",
+        "region": "Puglia"
+    },
+    {
+        "name": "Università del Salento",
+        "type": "Università",
+        "province": "Lecce",
+        "region": "Puglia"
+    },
+    {
+        "name": "Università degli studi della Basilicata",
+        "type": "Università",
+        "province": "Potenza",
+        "region": "Basilicata"
+    },
+    {
+        "name": "Università della Calabria",
+        "type": "Università",
+        "province": "Cosenza",
+        "region": "Calabria"
+    },
+    {
+        "name": "Università degli studi di Catanzaro - Magna Grecia",
+        "type": "Università",
+        "province": "Catanzaro",
+        "region": "Calabria"
+    },
+    {
+        "name": "Università degli studi Mediterranea di Reggio Calabria",
+        "type": "Università",
+        "province": "Reggio Calabria",
+        "region": "Calabria"
+    },
+    {
+        "name": "Università per stranieri Dante Alighieri di Reggio Calabria",
+        "type": "Università",
+        "province": "Reggio Calabria",
+        "region": "Calabria"
+    },
+    {
+        "name": "Università degli studi di Palermo",
+        "type": "Università",
+        "province": "Palermo",
+        "region": "Sicilia"
+    },
+    {
+        "name": "Istituto Superiore di Educazione Fisica di Palermo",
+        "type": "ISEF",
+        "province": "Palermo",
+        "region": "Sicilia"
+    },
+    {
+        "name": "Università degli studi di Messina",
+        "type": "Università",
+        "province": "Messina",
+        "region": "Sicilia"
+    },
+    {
+        "name": "Libera Università della Sicilia Centrale KORE di Enna",
+        "type": "Università",
+        "province": "Enna",
+        "region": "Sicilia"
+    },
+    {
+        "name": "Università degli studi di Catania",
+        "type": "Università",
+        "province": "Catania",
+        "region": "Sicilia"
+    },
+    {
+        "name": "Università degli studi di Sassari",
+        "type": "Università",
+        "province": "Sassari",
+        "region": "Sardegna"
+    },
+    {
+        "name": "Università degli studi di Cagliari",
+        "type": "Università",
+        "province": "Cagliari",
+        "region": "Sardegna"
+    }
+]
+
+export default universities;
