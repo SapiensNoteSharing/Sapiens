6714bdf6dd97bf6f99bdcb699dd2888c96eb90cb
Tiziano <tiziano.talini@stud.unifi.it>
2024-01-28 22:52:59 +0100

    fix load

--- src/lib/loadDatabase.js
+++ src/lib/loadDatabase.js
@@ -7,17 +7,25 @@ import { State, Region, Province, University } from '$lib/mongodb'
 export async function loadDatabase(){
     try{
         const country = await State.insertMany(countries.map(country => ({_id: country.countryId, name: country.countryName})), {ordered: false})
+    }catch(err){
+        console.log(err)
+    }
+    try{
         const regio = await Region.insertMany(regions.map(region => ({_id: region.code, name: region.name, state: 118})), {ordered: false})
+    }catch(err){
+        console.log(err)
+    }
+    try{
         const province = await Province.insertMany(provinces.map(province => ({_id: province.provinceId, name: province.provinceName, region: province.regionId, state: 118})), {ordered: false})
-        console.log('inserting')
+    }catch(err){
+        console.log(err)
+    }
+    try{
         universities.forEach(async university => {
-            console.log('inserting2')
             const region = await Region.findOne({name: university.region})
             const province = await Province.findOne({name: university.province})
             university.region = region?._id
             university.province = province?._id
-
-            console.log('unviersity', university)
 
             await University.findOneAndUpdate({name: university.name, type: university.type}, university, {upsert: true, new: true})
         })
