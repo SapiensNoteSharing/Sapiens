b3b5eef97fe29696b69513c436b178747fdf5a79
Tiziano <t.talini@libero.it>
2024-07-26 18:03:41 +0200

    remove warnings

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const ignored = [
+	'a11y-click-events-have-key-events', 
+	'css-unused-selector'
+]
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess()],
+	onwarn: (warning, handler) => {
+		if (ignored.includes(warning.code)) {
+			return;
+		}
+		console.log(warning.code)
+		handler(warning);
+	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
